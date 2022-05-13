e6f7906082e798fe9b8d7ab476561a4cfe1f5921
ccddan <ccdd.dev@gmail.com>
2022-05-13 11:42:14 -0500

    sm:feat: export contract info as json on contract deployment

--- smart-contracts/scripts/deploy.ts
+++ smart-contracts/scripts/deploy.ts
@@ -1,12 +1,42 @@
 import { ethers } from "hardhat";
+import { join } from "path";
+import { writeFileSync } from "fs";
 
 async function main() {
-  const Greeter = await ethers.getContractFactory("Greeter");
+  const contractName = "Greeter";
+  const Greeter = await ethers.getContractFactory(contractName);
   const greeter = await Greeter.deploy("Hello, Hardhat!");
 
   await greeter.deployed();
 
   console.log("Greeter deployed to:", greeter.address);
+
+  const exportFile = join(
+    __dirname,
+    "..",
+    "artifacts",
+    "contracts",
+    `${contractName}.sol`,
+    `${contractName}.addr.json`
+  );
+  const exportFileTimestamp = exportFile.replace(
+    ".addr.",
+    `.addr-${new Date().toISOString()}.`
+  );
+  const fileData = JSON.stringify(
+    { name: contractName, addr: greeter.address },
+    null,
+    2
+  );
+  writeFileSync(exportFile, fileData, { flag: "w" });
+  writeFileSync(exportFileTimestamp, fileData);
+
+  console.log(
+    "Contract info exported at:\n\t",
+    exportFile,
+    "\n\t",
+    exportFileTimestamp
+  );
 }
 
 main().catch((error) => {
