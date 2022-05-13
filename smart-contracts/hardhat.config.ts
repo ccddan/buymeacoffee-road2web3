0c2c9324c34e91a5dddadcc0e75d72420beea9cf
ccddan <ccdd.dev@gmail.com>
2022-05-13 13:39:35 -0500

    sc:feat: optimize solidity compilet

--- smart-contracts/hardhat.config.ts
+++ smart-contracts/hardhat.config.ts
@@ -25,7 +25,15 @@ const accounts =
     : [];
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
   defaultNetwork: "localhost",
   networks: {
     localhost: {
